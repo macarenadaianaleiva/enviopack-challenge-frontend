ed748a993f999aa629c1a1387fa43167c5d615e2
macarenadaianaleiva <macarenadaiana.leiva@gmail.com>
2025-08-06 20:47:39 -0300

    feat: setup redux store with cart and user slices

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,14 +1,5 @@
-import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-
-const NavbarWrapper = styled.nav`
-  background-color: #111;
-  color: #fff;
-  padding: 1rem;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
+import { NavbarWrapper } from './styles'
 
 const Navbar = () => {
   return (
